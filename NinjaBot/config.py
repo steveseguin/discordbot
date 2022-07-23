d80ed1e4136960b86113ba0f91577a95ea3d46f8
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2022-07-23 17:15:23 +0200

    improve config helper and add save feature

--- NinjaBot/config.py
+++ NinjaBot/config.py
@@ -1,17 +1,34 @@
 import aiofile
 import json
+import logging
 
 class Config:
     def __init__(self, file: str) -> None:
         self._configFile = file
-        self.botToken = None
+        self._configOptions = None
 
     async def parse(self):
         try:
             async with aiofile.async_open(self._configFile, mode="r") as f:
                 data = await f.read()
-            data = json.loads(data)
-            for key in data:
-                setattr(self, key, data[key])
+            self._configOptions = json.loads(data)
+        except Exception as E:
+            raise E
+
+    def get(self, key):
+        """return a config option by the given key"""
+        return self._configOptions[key]
+
+    async def set(self, key, newVal):
+        """set a config option to a new value + trigger flush"""
+        self._configOptions[key] = newVal
+        logging.debug(f"changed {key} to {newVal}")
+        await self._flushToFile()
+    
+    async def _flushToFile(self):
+        """Write config options from memory to file"""
+        try:
+            async with aiofile.async_open(self._configFile, mode="w") as f:
+                await f.write(json.dumps(self._configOptions, indent=4))
         except Exception as E:
             raise E
