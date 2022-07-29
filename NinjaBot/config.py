5f84a532ef853fd0cf0daafdb5dd8a27431dadaa
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2022-07-29 22:45:00 +0200

    refactor out file handling

--- NinjaBot/config.py
+++ NinjaBot/config.py
@@ -1,34 +1,28 @@
-import aiofile
-import json
 import logging
+from jsonFile import fileHelper
+
+logger = logging.getLogger("NinjaBot." + __name__)
 
 class Config:
+    """A helper class to handle the bot config file"""
     def __init__(self, file: str) -> None:
-        self._configFile = file
+        self._fh = fileHelper(file)
         self._configOptions = None
 
-    async def parse(self):
-        try:
-            async with aiofile.async_open(self._configFile, mode="r") as f:
-                data = await f.read()
-            self._configOptions = json.loads(data)
-        except Exception as E:
-            raise E
+    async def parse(self) -> None:
+        """read config file"""
+        self._configOptions = await self._fh.read()
 
     def get(self, key):
         """return a config option by the given key"""
         return self._configOptions[key]
 
-    async def set(self, key, newVal):
+    async def set(self, key, newVal) -> None:
         """set a config option to a new value + trigger flush"""
         self._configOptions[key] = newVal
-        logging.debug(f"changed {key} to {newVal}")
+        logger.debug(f"changed {key} to {newVal}")
         await self._flushToFile()
     
-    async def _flushToFile(self):
+    async def _flushToFile(self) -> None:
         """Write config options from memory to file"""
-        try:
-            async with aiofile.async_open(self._configFile, mode="w") as f:
-                await f.write(json.dumps(self._configOptions, indent=4))
-        except Exception as E:
-            raise E
+        self._fh.write(self._configOptions)
