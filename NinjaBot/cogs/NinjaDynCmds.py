d51e0d010f70d6cb86c023a3c2705e18fa5e732e
lebaston100 <26111114+lebaston100@users.noreply.github.com>
2022-07-29 22:45:36 +0200

    add dynamic command cog

--- NinjaBot/cogs/NinjaDynCmds.py
+++ NinjaBot/cogs/NinjaDynCmds.py
@@ -1,38 +1,78 @@
 import logging
-from discord.ext import commands
+import pathlib
+from discord.ext import commands, tasks
 from commandReplyProcessor import commandProc
+from jsonFile import fileHelper
 
 logger = logging.getLogger("NinjaBot." + __name__)
 
 class NinjaDynCmds(commands.Cog):
     def __init__(self, bot) -> None:
+        logger.debug(f"Loading {self.__class__.__name__}")
         self.bot = bot
         self.isInternal = False
-        self.commands = {"sampledynamiccommand": "and it's content"}
+        logger.debug(pathlib.Path(__file__).parent.resolve())
+        self._fh = fileHelper(pathlib.Path(__file__).parent.resolve() / "../suggestions.json")
+        self.commands = {}
+        self.loadCommands.start()
 
     async def process_command(self, ctx) -> bool:
         return await commandProc(self, ctx)
 
     @commands.command()
     @commands.has_role("Moderator")
-    async def add(self, ctx: commands.context, command: str, reply: str, *args) -> None: # use kwargs instead for reply
+    async def add(self, ctx: commands.context, command: str, reply: str, *args) -> None:
         """Command to dynamically add a command to the bot. Should not be used."""
+        args and await ctx.send("If you want to use spaces, please put the text in quotes")
+        await ctx.send("This is only for temp use. Please consider creating a PR to the bot repo.")
+
         logger.debug(command)
         logger.debug(reply)
-        logger.debug(args)
-        args and await ctx.send("If you want to use spaces, please put the text in quotes")
-        # TODO: re-integrate add command, but still warn user to also create a PR for it and run reload after merge
-        # for now just send a text message
-        await ctx.send("For now please create a PR against the bot repo to add a command and run !update after merge")
+
+        if command in self.commands:
+            await ctx.send("Command already exists as a temp command. please !delete <command> the command!")
+        else:
+            self.commands[command] = reply
+            await self._saveToFile()
+            await self._loadFromFile()
+            await ctx.send(f"Command '{command}' with reply '{reply}' has been added")
+
+    @commands.command()
+    @commands.has_role("Moderator")
+    async def delete(self, ctx: commands.context, command: str) -> None:
+        if command in self.commands:
+            del self.commands[command]
+            await self._saveToFile()
+            await self._loadFromFile()
+    
+    async def cog_command_error(self, ctx, error):
+        """Post error that happen inside this cog to channel"""
+        await ctx.send(error)
+
+    async def _loadFromFile(self) -> None:
+        logger.debug("Loading dyn cmds from file")
+        self.commands = await self._fh.read()
+        logger.debug(self.commands)
+
+    async def _saveToFile(self) -> None:
+        logger.debug("Saving dyn cmds to file")
+        await self._fh.write(self.commands)
+
+    # run task only once
+    @tasks.loop(count=1)
+    async def loadCommands(self) -> None:
+        await self._loadFromFile()
 
     async def getCommands(self) -> list:
         """Return the available commands as a list"""
         return list(self.commands.keys())
 
-async def setup(bot) -> None:
-    logger.debug(f"Loading {__name__}")
-    cogInstance = NinjaDynCmds(bot)
-    await bot.add_cog(cogInstance)
+    async def cog_unload(self) -> None:
+        logger.debug(f"Shutting down {self.__class__.__name__}")
 
-async def teardown(bot) -> None:
-    logger.debug(f"Shutting down {__name__}")
+async def setup(bot) -> None:
+    await bot.add_cog(NinjaDynCmds(bot))
+
+# TODO
+# add command to dict and save
+# add remove to remove from dict and save to file (+ reload from file)
